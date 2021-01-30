ded9d9c0728540bd2f3c2b8ee9fa877dfc6eb4ff
Anna Le <annale1501@gmail.com>
2021-01-30 15:10:11 -0500

    new files

--- ufswamphackathon2021/src/App.js
+++ ufswamphackathon2021/src/App.js
@@ -5,8 +5,8 @@ import Navbar from 'react-bootstrap/Navbar'
 import Home from './pages/Home/Home.js';
 import Interest from './pages/Interest/Interest.js';
 import How from './pages/How/How.js';
-import NotFound from "./pages/NotFound/NotFound.js";
 import Budgeting from "./pages/Budgeting/Budgeting.js";
+import Game from "./pages/Game/Game.js";
 import './App.css';
 import Header from "./components/Header/Header";
 import {
@@ -27,6 +27,7 @@ function App() {
             <Route exact path="/Interest" component={Interest} />
             <Route exact path="/Budgeting" component={Budgeting} />
             <Route exact path="/Tutorials" component={How} />
+            <Route exact path="/Game" component={Game} />
           </Switch>
       </div>
     </Router>
--- ufswamphackathon2021/src/pages/Interest/Interest.js
+++ ufswamphackathon2021/src/pages/Interest/Interest.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function Interest() {
+    const [annualIncome, setAnnualIncome] = useState(0) 
+    const [loanBalance, setLoanBalance] = useState(0)
+    const [days, setDays] = useState(0)
+    const [result, setResult] = useState(0)
+    
+    const calculate = (e) => {
+        e.preventDefault();
+        var decimal = annualIncome/100;
+        var annualInt = decimal/365;
+        var dailyInt = annualInt * loanBalance;
+        var monthlyInt = dailyInt * 30;
+        setResult(monthlyInt);
+
+    }
+
+
     return (
+        
         <div>
-            <h1>This is the investments page</h1>
+            <h1>Interest Page</h1>
+            <form onSubmit = {calculate}>
+                <p>Input annual interest</p>
+                <input onChange = {e => setAnnualIncome(e.target.value)} value = {annualIncome}  ></input>
+                <br></br>
+                <p>Input loan balance</p>
+                <input onChange = {e => setLoanBalance(e.target.value)} value = {loanBalance}  ></input>
+                <br></br>
+                <p>Input days since last payments</p>
+                <input onChange = {e => setDays(e.target.value)} value = {days}  ></input>
+                <br></br>
+                <br></br>
+                <button>Submit</button>
+            </form>
+            <p2>Interest occured since last payment: {result}</p2>
         </div>
     )
 }
