0e20c9428453f23db508c4fff69ee05da291a465
Anna Le <annale1501@gmail.com>
2021-01-30 05:10:40 -0500

    ui changes

--- ufswamphackathon2021/src/Budget.js
+++ ufswamphackathon2021/src/Budget.js
@@ -2,28 +2,45 @@ import React, { useState } from 'react';
 import './Budget.css';
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from '@material-ui/core/TextField';
+import moment from 'react-moment';
+import FormControl from '@material-ui/core/FormControl';
+
+
 
 export default function Interest() {
+    const classes = useStyles();
     const [totalBalance, setTotalBalance] = useState(0);
     const [displayBalance, setDisplayBalance] = useState(0);
-    //const [categories, setCategories] = useState(['Groceries', 'Entertainment', 'Bills', 'Emergency']);
     const [name, setName] = useState('');
     const [percentage, setPercentage] = useState(0);
     const [categories, setCategories] = useState([]);
     const [totalPercent, setTotalPercent] = useState(0);
     const [warning, setWarning] = useState('')
     const [spent, setSpent] = useState(0);
+    const [pickSpent, setPickSpent] = useState('');
+    const [dropdownList, setDropdownList] = useState([]);
+
+    
 
     const options = [
         'groceries', 'entertainment', 'emergency'
       ];
-      const defaultOption = options[0];
 
     const handleSubmit = (e) => {
         e.preventDefault();
         setDisplayBalance(totalBalance);
+        console.log("WORKING");
 
     }
+
+    const handleChoice = (event) => {
+        setPickSpent(event.target.value);
+      };
 
     const handleCategories = (e) => {
         e.preventDefault();
@@ -34,40 +51,64 @@ export default function Interest() {
         if(currentPercent <= 100) {
             setTotalPercent(currentPercent);
             setCategories(oldList => [...oldList, {name: name, percentage: percentage, catBalance: calcAmount}]);
+            setDropdownList(oldList => [...oldList, name]);
+            console.log(dropdownList);
         } else {
             setWarning("Check Value or 100% Reached.");
         }
-    
-        //setCategories(list => list.concat(name));
+
     }
 
     const spentFunction = (e) => {
         e.preventDefault();
         console.log("enters");
+        // Object.keys(categories).map(function(key,index) {
+        //     console.log(categories[key].name)
+        // })
     }
+
 
 
     //object for 
 
     return (
         <div className="budget">
-            <h1>Budget Page</h1>
+            <p className="title">BUDGET PAGE</p>
             <form onSubmit={handleSubmit}>
-                <p>Set Total Balance:</p>
-                <input onChange={e => setTotalBalance(parseInt(e.target.value))} value={totalBalance}></input>
-                <button>Submit</button>
+                <p className="headings">SET TOTAL BALANCE</p>
+                <input className="input" onChange={e => setTotalBalance(parseInt(e.target.value))} value={totalBalance}></input>
+                <Button variant="outlined" color="secondary" onClick={handleSubmit}>Submit</Button>
             </form>
             <form className="cat" onSubmit={handleCategories}>
-                <p>Input Categories</p>
-                <input placeholder="category" onChange={cat => setName(cat.target.value)} value={name}></input>
-                <input placeholder="percentage" onChange={per => setPercentage(parseInt(per.target.value))} value={percentage}></input>
-                <button>Submit</button>
+                <p className="headings">ADD CATEGORIES</p>
+                <input className="input" onChange={cat => setName(cat.target.value)} value={name}></input>
+                <input className="input" onChange={per => setPercentage(parseInt(per.target.value))} value={percentage}></input>
+                <Button variant="outlined" color="secondary" onClick={handleCategories}>Submit</Button>
             </form>
-            <form onSubmit={spentFunction}>
-                <p>Spent</p>
-                <input placeholder="spent" onChange={num => setSpent(num.target.value)} value={spent}></input>
-                <Dropdown className="test" options={options} value={defaultOption} placeholder="Select an option"/>
-                <button>Submit</button>
+            <form className="spending" onSubmit={spentFunction}>
+    
+                {/* <Dropdown className="test" options={dropdownList} onChange={e => setPickSpent(e)} placeholder="Select an option"/> */}
+                <div className={classes.root}>
+                    <p className="headings">SPENT</p>
+                    <input className="input" onChange={num => setSpent(num.target.value)} value={spent}></input>
+                    <Select
+                        labelId="demo-simple-select-helper-label"
+                        id="demo-simple-select-helper"
+                        className={classes.selectEmpty}
+                        value={pickSpent}
+                        onChange={handleChoice}
+                        >
+                        <MenuItem value="">
+                            <em>None</em>
+                        </MenuItem>
+                        {categories.map((catItem, index) =>
+                            <MenuItem value={catItem.name}>{catItem.name}</MenuItem>
+                        )}
+                
+                    </Select>
+                    <Button variant="outlined" color="secondary" onClick={handleSubmit}>Submit</Button>
+                </div>
+               
             </form>
             <p>{displayBalance}</p>
 
@@ -76,9 +117,31 @@ export default function Interest() {
                     <p>Category: {catItem.name} Percentage: {catItem.percentage} Amount: {catItem.catBalance} </p>
                 )}
                 <p>{warning}</p>
+                <p>{pickSpent}</p>
             </div>
         </div>
     )
 }
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120
+    },
+    
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+      width: 200,
+      borderColor: 'red'
+    },
+    multilineColor:{
+        color:'red',
+    
+    },
+    specialOutline: {
+        borderColor: 'red',
+        color: 'red'
+      },
+  }));
 
 
