62afba2f242d39cf74180796e90e509d666a812e
Anna Le <annale1501@gmail.com>
2021-01-30 13:27:59 -0500

    more ui changes

--- ufswamphackathon2021/src/Budget.js
+++ ufswamphackathon2021/src/Budget.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Budget.css';
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
@@ -6,43 +6,42 @@ import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from "@material-ui/core/styles";
-import TextField from '@material-ui/core/TextField';
-import moment from 'react-moment';
-import FormControl from '@material-ui/core/FormControl';
 import { PieChart } from 'react-minimal-pie-chart';
+import moment from 'moment';
 
 
 export default function Interest() {
     const classes = useStyles();
-    const [totalBalance, setTotalBalance] = useState(0);
+    const [totalBalance, setTotalBalance] = useState('');
     const [displayBalance, setDisplayBalance] = useState(0);
     const [name, setName] = useState('');
-    const [percentage, setPercentage] = useState(0);
+    const [percentage, setPercentage] = useState('');
     const [categories, setCategories] = useState([]);
     const [totalPercent, setTotalPercent] = useState(0);
     const [warning, setWarning] = useState('')
-    const [spent, setSpent] = useState(0);
+    const [spent, setSpent] = useState('');
     const [pickSpent, setPickSpent] = useState('');
     const [dropdownList, setDropdownList] = useState([]);
     const [pieChartData, setPieChartData] = useState([]);
     var colors = ['#fad15f', '#f043da', '#82f4ff'];
     const [colorCount, setColorCount] = useState(0);
+    const [isSpentSubmitted, setIsSpentComitted] = useState(false);
+    const [date, setDate] = useState('');
     
 
-    const options = [
-        'groceries', 'entertainment', 'emergency'
-      ];
+    useEffect(() => { 
+        setDate(moment().format("MMMM DD, Y"));
+    });
 
     const handleSubmit = (e) => {
         e.preventDefault();
         setDisplayBalance(totalBalance);
-        console.log("WORKING");
 
     }
 
-    const handleChoice = (event) => {
-        setPickSpent(event.target.value);
-      };
+    // const handleChoice = (event) => {
+    //     setPickSpent(event.target.value);
+    //   };
 
     const handleCategories = (e) => {
         e.preventDefault();
@@ -60,16 +59,26 @@ export default function Interest() {
         } else {
             setWarning("Check Value or 100% Reached.");
         }
-
     }
 
-    const spentFunction = (e) => {
-        e.preventDefault();
-        console.log("enters");
-        // Object.keys(categories).map(function(key,index) {
-        //     console.log(categories[key].name)
-        // })
+      const handleSpent = (e) => {
+        setIsSpentComitted(true);
     }
+
+    useEffect(() => {
+        if (isSpentSubmitted) {
+            Object.keys(categories).map(function(key,index) {
+                if(pickSpent == categories[key].name && categories[key].catBalance - spent >= 0) {
+                    categories[key].catBalance  = categories[key].catBalance - spent;
+                    console.log(categories[key].catBalance);
+                    console.log(categories);
+                }
+            setIsSpentComitted(false);
+           })
+
+        }
+    }, [isSpentSubmitted]);
+
 
 
 
@@ -78,66 +87,73 @@ export default function Interest() {
     return (
         <div className="budget">
             <p className="title">BUDGET PAGE</p>
-            <form onSubmit={handleSubmit}>
-                <p className="headings">SET TOTAL BALANCE</p>
-                <input className="input"  onChange={e => setTotalBalance(parseInt(e.target.value))} value={totalBalance}></input>
-                <Button variant="outlined" color="secondary" onClick={handleSubmit}>Submit</Button>
-            </form>
-            <form className="cat" onSubmit={handleCategories}>
-                <p className="headings">ADD CATEGORIES</p>
-                <input className="input" onChange={cat => setName(cat.target.value)} value={name}></input>
-                <input className="input" onChange={per => setPercentage(parseInt(per.target.value))} value={percentage}></input>
-                <Button variant="outlined" color="secondary" onClick={handleCategories}>Submit</Button>
-            </form>
-            <form className="spending" onSubmit={spentFunction}>
-    
-                {/* <Dropdown className="test" options={dropdownList} onChange={e => setPickSpent(e)} placeholder="Select an option"/> */}
-                <div className={classes.root}>
-                    <p className="headings">SPENT</p>
-                    <input className="input" onChange={num => setSpent(num.target.value)} value={spent}></input>
-                    <Select
-                        labelId="demo-simple-select-helper-label"
-                        id="demo-simple-select-helper"
-                        className={classes.selectEmpty}
-                        value={pickSpent}
-                        inputProps={{
-                            classes: {
-                                icon: classes.icon,
-                            },
-                        }}
-                        onChange={handleChoice}
-                        >
-                        <MenuItem value="">
-                            <em>Select</em>
-                        </MenuItem>
-                        {categories.map((catItem, index) =>
-                            <MenuItem value={catItem.name}>{catItem.name}</MenuItem>
-                        )}
-                
-                    </Select>
+            <p className="date">{date}</p>
+        <div className="topContainer">
+         <div className="formArea">
+                <form onSubmit={handleSubmit}>
+                    <p className="headings">SET TOTAL BALANCE</p>
+                    <input className="input"  onChange={e => setTotalBalance(parseInt(e.target.value))} value={totalBalance}></input>
                     <Button variant="outlined" color="secondary" onClick={handleSubmit}>Submit</Button>
-                </div>
-               
-            </form>
-            <p>{displayBalance}</p>
-
-            <div>
-                {categories.map((catItem, index) =>
-                    <p>Category: {catItem.name} Percentage: {catItem.percentage} Amount: {catItem.catBalance} </p>
-                )}
-                <p>{warning}</p>
-                <p>{pickSpent}</p>
+                </form>
+                <form className="cat" onSubmit={handleCategories}>
+                    <p className="headings">ADD CATEGORIES</p>
+                    <input className="input" onChange={cat => setName(cat.target.value)} value={name}></input>
+                    <input className="input" onChange={per => setPercentage(parseInt(per.target.value))} value={percentage}></input>
+                    <Button variant="outlined" color="secondary" onClick={handleCategories}>Submit</Button>
+                </form>
+                <form className="spending" onSubmit={handleSpent}>
+                    {/* <Dropdown className="test" options={dropdownList} onChange={e => setPickSpent(e)} placeholder="Select an option"/> */}
+                    <div className={classes.root}>
+                        <p className="headings">SPENT</p>
+                        <input className="input" onChange={num => setSpent(num.target.value)} value={spent}></input>
+                        <Select
+                            labelId="demo-simple-select-helper-label"
+                            id="demo-simple-select-helper"
+                            className={classes.selectEmpty}
+                            value={pickSpent}
+                            inputProps={{
+                                classes: {
+                                    icon: classes.icon,
+                                },
+                            }}
+                            onChange={choice => setPickSpent(choice.target.value)}
+                            >
+                            <MenuItem value="">
+                                <em>Select</em>
+                            </MenuItem>
+                            {categories.map((catItem, index) =>
+                                <MenuItem value={catItem.name}>{catItem.name}</MenuItem>
+                            )}
+                        </Select>
+                        <Button variant="outlined" color="secondary" onClick={handleSpent}>Submit</Button>
+                    </div>
+                </form>
             </div>
-           
+             <div className="infoArea">
+                    <p className="headings">TOTAL BALANCES: ${displayBalance}</p>
+                    <div className="catInfo">
+                        {categories.map((catItem, index) =>
+                            <p>Category: {catItem.name} | Balance: ${catItem.catBalance} </p>
+                            //<p>Category: {catItem.name} | Amount: ${catItem.catBalance} | Percentage: {catItem.percentage}</p>
+                        )}
+                        <p className="warning">{warning}</p>
+                    </div>
+             </div>
+        </div>
+        
 
-            <div>
+            <div className="pieChart">
                 <PieChart
-                    radius={10}
+                    radius={25}
+                    viewBoxSize={[300, 100]}
+                    center={[150, 40]}
                     data={
                         pieChartData 
-                    }
-                    />;
+                    }/>
             </div>
+
+    
+
             
         </div>
     )
@@ -148,19 +164,20 @@ const useStyles = makeStyles((theme) => ({
       margin: theme.spacing(1),
       minWidth: 120
     },
-    
     selectEmpty: {
         width: 200,
         marginRight: 10,
-    "&:before": {
-        borderColor: "blueviolet",
-        //width: 200,
-      }, 
-      '&:after': {
-        borderColor: "blueviolet",
-    }
-    }, icon: {
-        fill: "blueviolet",
+        color: "white",
+        "&:before": {
+            borderColor: "blueviolet",
+            //width: 200,
+        }, 
+        '&:after': {
+            borderColor: "blueviolet",
+        },
+        icon: {
+            fill: "blueviolet",
+        }
     },
     multilineColor:{
         color:'red',
@@ -169,6 +186,17 @@ const useStyles = makeStyles((theme) => ({
     specialOutline: {
         borderColor: 'red',
         color: 'red'
+      },
+      cardRoot: {
+        width: 300,
+        height: 300,
+        backgroundColor: '#8a2be2',
+        color: 'white'
+      },
+      title: {
+        fontSize: 20,
+        padding: 10,
+        letterSpacing: 3
       },
   }));
 
