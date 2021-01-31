ced81da7d00116f02dd2e7f65b29026c8bc35f5c
Matina <matinasaura@gmail.com>
2021-01-30 22:25:00 -0500

    dont merge this

--- ufswamphackathon2021/src/pages/Tutorials/Tutorials.js
+++ ufswamphackathon2021/src/pages/Tutorials/Tutorials.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import './Tutorials.css';
-import { Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Bank from "./bank/Bank.js";
 
 export default function Tutorials() {
     return (
         <div className="tutorialPage">
           <p className="title">Tutorials</p>
            <div className = "module_Tutorials">
-
-              <div className = "module_Card"><Link to="/Credit" className = "module_Cards">Credit and Debit 101</Link></div>
+              <div className = "module_Card"><Route exact path="/credit_debit" component={Bank} /> Credit 101</div>
               <div className = "module_Card"><Link to="/Taxes" className = "module_Cards">Taxes 101</Link></div>
               <div className = "module_Card"><Link to="/Bank" className = "module_Cards">How To: Banking</Link></div>
-              <div className = "module_Card"><Link to="/Checks" className = "module_Cards">How To: Checks and Bills</Link></div>
-              
+              <div className = "module_Card"><Link to="/Checks" className = "module_Cards">How To: Checks and Bills</Link></div>             
            </div>
            <div className="checkContainer">
                <div className="check">
--- ufswamphackathon2021/src/pages/Tutorials/credit_debit/Credit.js
+++ ufswamphackathon2021/src/pages/Tutorials/credit_debit/Credit.js
@@ -3,24 +3,9 @@ import './Credit.css';
 
 export default function Credit() {
     return (
-        <div className="tutorialPage">
+        <div className="creditPage">
           <p className="title">Tutorials</p>
            <div>
-              <div className="modules">
-              <p className = "title" >Credit and Debit 101</p>
-              </div>
-
-              <div className="modules">
-              <p className = "title" >Taxes 101</p>
-              </div>
-
-              <div className="modules">
-              <p className = "title" >How To: Banking</p>
-              </div>
-
-              <div className="modules">
-              <p className = "title" >How To: Checks and Bills</p>
-              </div>
               
            </div>
            <div className="checkContainer">
